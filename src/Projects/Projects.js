9c4d86def0b60c73c1e620c2b6a8b5f2fc02bc10
Yaroslav Karpash <ykarpash@gmail.com>
2023-07-24 17:05:49 +0300

    Pet react projects

---
+++ src/Projects/Projects.js
@@ -0,0 +1,12 @@
+import './Projects.css';
+
+function Projects() {
+   return (
+<div className="Sidebar-site">
+   <h1>React Project</h1>
+   <p>I am projects side</p>
+</div>
+   )
+}
+
+export default Projects;
